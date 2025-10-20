0e7be192bb8a3a7385f4d995b5cf4417d4306682
brickphone <93726614+brickphone@users.noreply.github.com>
2025-10-20 14:55:14 +0200

    Some progress

--- client/.next/static/chunks/app/layout-097c94cc97fa7e19.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{157:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,3520,23))},3520:()=>{}},e=>{e.O(0,[253,839,887,358],()=>e(e.s=157)),_N_E=e.O()}]);
